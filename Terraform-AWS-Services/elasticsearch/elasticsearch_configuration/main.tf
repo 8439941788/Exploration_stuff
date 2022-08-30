eaab0193163b3846a6881a1924cede4210b19a46
Amit Joshi <55866283+8439941788@users.noreply.github.com>
2022-08-31 00:03:09 +0530

    Update main.tf

--- Terraform-AWS-Services/elasticsearch/elasticsearch_configuration/main.tf
+++ Terraform-AWS-Services/elasticsearch/elasticsearch_configuration/main.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-devopsthehardway"
-    key    = "eks-terraform-workernodes.tfstate"
-    region = "ap-south-1"
-  }
+#terraform {
+ # backend "s3" {
+  #  bucket = "terraform-state-devopsthehardway"
+   # key    = "eks-terraform-workernodes.tfstate"
+    #region = "ap-south-1"
+  #}
   required_providers {
     aws = {
       source = "hashicorp/aws"
