14db8acc0ccad11d615e1a7b028b76a67242e87c
Amit Joshi <55866283+8439941788@users.noreply.github.com>
2022-08-30 23:45:21 +0530

    Update variable.tf

--- Terraform-AWS-Services/elasticsearch/elasticsearch_configuration/variable.tf
+++ Terraform-AWS-Services/elasticsearch/elasticsearch_configuration/variable.tf
@@ -1,9 +1,9 @@
 variable "subnet_id_1" {
   type = string
-  default = "subnet-07d002bb7e32b67fc"
+  default = "subnet-0424a9fef5acaf958"
 }
 
 variable "subnet_id_2" {
   type = string
-  default = "subnet-03c49bbe3a085ef44"
+  default = "subnet-06ed1c99527f571bf"
 }
