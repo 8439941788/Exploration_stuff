b6bfce01158e88ecc874384879f39d2683857d85
Amit Joshi <55866283+8439941788@users.noreply.github.com>
2022-08-31 16:40:35 +0530

    Update variable.tf

--- Terraform-AWS-Services/elasticsearch/elasticsearch_configuration/variable.tf
+++ Terraform-AWS-Services/elasticsearch/elasticsearch_configuration/variable.tf
@@ -7,3 +7,18 @@ variable "subnet_id_2" {
   type = string
   default = "subnet-0ce643ea15af3c1d3"
 }
+
+variable "aws_region" {
+  description = "The AWS region things are created in"
+  default     = "ap-south-1"
+}
+
+variable "access_key" {
+  type    = string
+  default = "AKIAXIZEO4L3N4RN4BYB"
+}
+
+variable "secret_key" {
+  type    = string
+  default = "Wvk6Pu80TbcyR47cP4qleo4uIeraIhZQS1hCxaFp"
+}
