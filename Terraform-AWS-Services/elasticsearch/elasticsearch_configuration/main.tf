7634016bad2494f9554326571cab07b36f807dd3
Amit Joshi <55866283+8439941788@users.noreply.github.com>
2022-08-31 16:44:55 +0530

    Update main.tf

--- Terraform-AWS-Services/elasticsearch/elasticsearch_configuration/main.tf
+++ Terraform-AWS-Services/elasticsearch/elasticsearch_configuration/main.tf
@@ -3,19 +3,6 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
 }
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-devopsthehardway-test"
-    key    = "eks-terraform-workernodes.tfstate"
-    region = "ap-south-1"
-  }
-    required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.32.0"
-    }
-  }
-}
 
 
 # IAM Role for EKS to have access to the appropriate resources
